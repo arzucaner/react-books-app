29bd2380368a7bda47e7b8730f5f85802dddbeb0
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-03 23:15:40 +0000

    Add header

---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+import react from "react";
+const Main=()=>{
+    return(
+        <>
+           <div className="header">
+                <div className="row1">
+                     <h1>Sometimes books are the best friends. It doesn't judge you, it just teaches you...</h1>  
+                </div>
+                <div className="row2">
+                    <h2>Find Your Book</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"/>
+                    </div>
+               
+                </div>
+            </div>
+        </>
+    )
+}
+export default Main;    
