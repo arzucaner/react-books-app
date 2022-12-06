70f568618ebd2f12ff1714a2dbcceefcbe5ba80a
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-06 00:01:33 +0000

    Added card.js

---
+++ src/components/card.js
@@ -0,0 +1,15 @@
+import react from "react";
+const Card=()=>{}
+    return(
+        <>
+            <div className="card">
+            <img src="./images/book.png" alt=""  />
+            <div className="bottom">
+                 <h3 className="title"> React JS</h3>
+                 <p className="amont">&#8377;3290</p>
+            </div>
+            </div>
+        </>
+    )
+}
+export default Card;
