623b99b9a9fab6388044e2ee38aea1ecea4340dc
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-19 23:22:17 +0000

    Update card

--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,28 @@
 import react from "react";
 const [show,setShow]=useState(false);
 const [bookItem,setItem]=useState();
-const Card = ({books}) => { 
+const Card = ({book}) => { 
 console.log (book)
-return (
-        <>
-            {   
-                 book.map((item) => {
-                     let thumbnail=item.volumeInfo.imageLinks && item.volumInfo.imageLinks.smallThumbnail;
-                     let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-                     if(thumbnail!= undefined && amount !=undefined)
-                     {
-                        return (
-                            <>
-                            <div className="card" onClick={()=>{setShow(true);setItem(utem)}}>                      
+  
+         
+                 book.map((item) => {                       
+                 let thumbnail=item.volumeInfo.imageLinks && item.volumInfo.imageLinks.smallThumbnail;
+                 let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+                 if(thumbnail!= undefined && amount !=undefined)
+                 {
+                    return (
+                        <>
+                        <div className="card" onClick={()=>{setShow(true);setItem(utem)}}>                      
                             <img src="{thumbnail}" alt=""  />
                             <div className="bottom">
-                                <h3 className="title"> {item.volumeInfo.title}</h3>
-                                <p className="amont">&#8377;{"amount"}</p>
+                                <h3 className="title">{item.volumeInfo.title}</h3>
+                                <p className="amont">&#8377;{amount}</p>
               </div>
             </div>
               <Modal show={show} item={bookItem} onClose={}=>setShow(false)/>
         </>
     )
+}
+}
 }
 export default Card;
