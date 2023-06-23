5098553b902f7bea31ef70f640ecc7779ca50d49
Arzu Caner <arzuguneycaner@gmail.com>
2023-06-23 15:15:48 +0100

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -2,24 +2,34 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 
 const Card = ({ book }) => {
-  const [show, setShow] = useState(false);
-  const [bookItem, setItem] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleCardClick = (book) => {
+    setShowModal(true);
+    setSelectedBook(book);
+  };
 
   return (
     <>
       {book.map((item) => {
-        const thumbnail =
-          item.volumeInfo.imageLinks &&
-          item.volumeInfo.imageLinks.smallThumbnail;
-        const amount =
-          item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-        const currency =
-          item.saleInfo.listPrice && item.saleInfo.listPrice.currencyCode;
-        const formattedAmount =
-          amount && new Intl.NumberFormat("en-GB", { style: "currency", currency }).format(amount);
+        const thumbnail = item.volumeInfo.imageLinks?.smallThumbnail;
+        const amount = item.saleInfo.listPrice?.amount;
+        const currency = item.saleInfo.listPrice?.currencyCode;
+        const formattedAmount = amount
+          ? new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency,
+          }).format(amount)
+          : null;
+
         if (thumbnail && amount) {
           return (
-            <div className="card" key={item.id} onClick={() => { setShow(true); setItem(item) }}>
+            <div
+              className="card"
+              key={item.id}
+              onClick={() => handleCardClick(item)}
+            >
               <img src={thumbnail} alt="" />
               <div className="bottom">
                 <h3 className="title">{item.volumeInfo.title}</h3>
@@ -31,7 +41,13 @@ const Card = ({ book }) => {
           return null;
         }
       })}
-      <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
+      {showModal && (
+        <Modal
+          show={showModal}
+          item={selectedBook}
+          onClose={() => setShowModal(false)}
+        />
+      )}
     </>
   );
 };
